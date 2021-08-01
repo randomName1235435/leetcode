9afe78d78584397624accca3df8f3284de9238d4
mm <unam3d@gmx.de>
2021-08-02 00:18:18 +0200

    added length of longes substring

---
+++ leetCode/LengthOfLongestSubstringClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class LengthOfLongestSubstringClass
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s == null || s == String.Empty)
+                return 0;
+
+            HashSet<char> set = new HashSet<char>();
+            int currentMax = 0,
+                i = 0,
+                j = 0;
+
+            while (j < s.Length)
+                if (!set.Contains(s[j]))
+                {
+                    set.Add(s[j++]);
+                    currentMax = Math.Max(currentMax, j - i);
+                }
+                else
+                    set.Remove(s[i++]);
+
+            return currentMax;
+        }
+    }
+}
