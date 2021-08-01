031166bb2e488060136860ca0b1335cb6df62cc8
mm <unam3d@gmx.de>
2021-08-01 21:25:49 +0200

    added addtwonumbers

---
+++ leetCode/AddTwoNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class AddTwoNumbersClass
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode first = result;
+            int overflow = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                int sum = overflow;
+
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                if (sum >= 10)
+                {
+                    overflow = sum / 10;
+                    sum = sum % 10;
+                }
+                else
+                    overflow = 0;
+
+                result.next = new ListNode(sum);
+                result = result.next;
+            }
+
+            if (overflow > 0)
+                result.next = new ListNode(overflow);
+            return first = first.next;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ leetCodeTest/AddTwoNumbersClassTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using leetCode;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class AddTwoNumbersClassTest
+    {
+
+        [TestMethod]
+        public void AddTwoNumbersZeroTest()
+        {
+            AddTwoNumbersClass instance = new AddTwoNumbersClass();
+            var firstParam = new ListNode (0);
+            var secondParam = new ListNode(0);
+            var result = instance.AddTwoNumbers (firstParam, secondParam);
+            Assert.IsTrue(result.val == 0) ;
+        }
+        [TestMethod]
+        public void AddTwoNumbersZeroTestExample()
+        {
+            AddTwoNumbersClass instance = new AddTwoNumbersClass();
+            var firstParam = new ListNode(2,new ListNode( 4, new ListNode( 3)));
+            var secondParam = new ListNode(5, new ListNode(6, new ListNode(4)));
+            var result = instance.AddTwoNumbers(firstParam, secondParam);
+            Assert.IsTrue(result.val == 7 && result.next.val == 0 && result.next.next.val == 8);
+        }
+    }
+}
