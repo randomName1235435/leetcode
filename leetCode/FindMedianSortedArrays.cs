efdf625ddc2e8bde6e9cab36aaff71da3b293b1b
mm <unam3d@gmx.de>
2021-08-02 10:37:11 +0200

    added FindMedianSortedArrays

---
+++ leetCode/FindMedianSortedArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class FindMedianSortedArraysClass
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var sorted = new List<int>(nums1);
+            sorted.AddRange(nums2);
+            sorted.Sort();
+            if (sorted.Count() % 2 == 1) return sorted[(sorted.Count() / 2)];
+            else
+            {
+                var first = sorted[(sorted.Count() - 1) / 2];
+                var second = sorted[(sorted.Count()) / 2];
+                float result = (first + second) / 2f;
+                return result;
+            }
+        }
+    }
+}
---
+++ leetCodeTest/FindMedianSortedArraysClassTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class FindMedianSortedArraysClassTest
+    {
+        [TestMethod]
+        public void FindMedianSortedArraysThreeTotalNumbsSimple()
+        {
+            FindMedianSortedArraysClass instance = new FindMedianSortedArraysClass();
+            var result = instance.FindMedianSortedArrays(new int[] { 1,3}, new int[] { 2});
+            Assert.IsTrue(result == 2);
+        }
+        [TestMethod]
+        public void FindMedianSortedArraysFourTotalNumbsSimple()
+        {
+            FindMedianSortedArraysClass instance = new FindMedianSortedArraysClass();
+            var result = instance.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3,4 });
+            Assert.IsTrue(result == 2.5f);
+        }
+    }
+}
