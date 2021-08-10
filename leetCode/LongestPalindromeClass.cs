fa25d3a1fa60a2b8fc8cb3975b9995c59db310d6
mm <unam3d@gmx.de>
2021-08-10 09:15:12 +0200

    added longestpalindrome

---
+++ leetCode/LongestPalindromeClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class LongestPalindromeClass
+    {
+        public string LongestPalindrome(string param) {
+            string longestPalindrome = "";
+            int k = 0;
+            for (int i = 0; i < param.Length; i++)
+            {
+                if (longestPalindrome.Length > param.Substring(i).Length)
+                    return longestPalindrome;
+                k = 1;
+                while (k+i <= param.Length)
+                {
+                    string currentPalindromeCandidate = param.Substring(i, k);
+                    if (IsPalindrome(currentPalindromeCandidate))
+                    {
+                        string currentPalindrome = currentPalindromeCandidate;
+                        if (currentPalindrome.Length > longestPalindrome.Length)
+                        {
+                            longestPalindrome = currentPalindrome;
+                        }
+                    }
+                    k++;
+                }
+            }
+            return longestPalindrome;
+        }
+
+        public bool IsPalindrome(string currentPalindromeCandidate)
+        {
+            for (int i = 0; i < currentPalindromeCandidate.Length/2; i++)
+            {
+                if (currentPalindromeCandidate[i] != currentPalindromeCandidate[currentPalindromeCandidate.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+
---
+++ leetCodeTest/LongestPalindromeClassTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class LongestPalindromeClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("babad");
+            Assert.IsTrue(result == "bab" || result == "aba");
+        }
+        [TestMethod]
+        public void OnlyOneCharPalindrome()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("aaaaab");
+            Assert.IsTrue(result == "aaaaa");
+        }
+        [TestMethod]
+        public void OnlyTwoCharPalindrome()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("bbaabaa");
+            Assert.IsTrue(result == "aabaa");
+        }
+        [TestMethod]
+        public void IsPalindromeSimpleTestWithPalindrome()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.IsPalindrome("aabaa");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void IsPalindromeSimpleTestWithNoPalindrome()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.IsPalindrome("aaabaa");
+            Assert.IsTrue(!result);
+        }
+        [TestMethod]
+        public void IsPalindromeBigParam()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            Assert.IsTrue(result == "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+        }
+        [TestMethod]
+        public void IsPalindromeOneSingleChar()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("a");
+            Assert.IsTrue(result == "a");
+        }
+        [TestMethod]
+        public void IsPalindromeComplicatedLongParam()
+        {
+            LongestPalindromeClass instance = new LongestPalindromeClass();
+            var result = instance.LongestPalindrome("jglknendplocymmvwtoxvebkekzfdhykknufqdkntnqvgfbahsljkobhbxkvyictzkqjqydczuxjkgecdyhixdttxfqmgksrkyvopwprsgoszftuhawflzjyuyrujrxluhzjvbflxgcovilthvuihzttzithnsqbdxtafxrfrblulsakrahulwthhbjcslceewxfxtavljpimaqqlcbrdgtgjryjytgxljxtravwdlnrrauxplempnbfeusgtqzjtzshwieutxdytlrrqvyemlyzolhbkzhyfyttevqnfvmpqjngcnazmaagwihxrhmcibyfkccyrqwnzlzqeuenhwlzhbxqxerfifzncimwqsfatudjihtumrtjtggzleovihifxufvwqeimbxvzlxwcsknksogsbwwdlwulnetdysvsfkonggeedtshxqkgbhoscjgpiel");
+            Assert.IsTrue(result == "sknks");
+        }
+    }
+}
