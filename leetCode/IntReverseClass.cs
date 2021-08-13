0c652e415c0fbdb01e9820786515383c05fc3eec
mm <unam3d@gmx.de>
2021-08-13 17:26:00 +0200

    added intreverse

---
+++ leetCode/IntReverseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class IntReverseClass
+    {
+        public int Reverse(int param)
+        {
+            int ret = 0;
+            while (param != 0)
+            {
+                int pop = param % 10;
+                param /= 10;
+                if (ret > int.MaxValue / 10 || (ret == int.MaxValue / 10 && pop > 7)) return 0;
+                if (ret < int.MinValue / 10 || (ret == int.MinValue / 10 && pop < -8)) return 0;
+                ret = ret * 10 + pop;
+            }
+            return ret;
+        }
+    }
+}
---
+++ leetCodeTest/IntReverseClassTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class IntReverseClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new IntReverseClass();
+            var result = instance.Reverse(123);
+            Assert.IsTrue(result == 321);
+        }
+        [TestMethod]
+        public void MaxValue()
+        {
+            var instance = new IntReverseClass();
+            var result = instance.Reverse(int.MaxValue);
+            Assert.IsTrue(result == 0);
+        }
+        [TestMethod]
+        public void MinValue()
+        {
+            var instance = new IntReverseClass();
+            var result = instance.Reverse(int.MinValue);
+            Assert.IsTrue(result == 0);
+        }
+        [TestMethod]
+        public void ZeroFirst()
+        {
+            var instance = new IntReverseClass();
+            var result = instance.Reverse(120);
+            Assert.IsTrue(result == 21);
+        }
+    }
+}
