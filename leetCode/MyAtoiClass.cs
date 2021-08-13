b2a193e48d0fd8bec87d290ec8a2edd129781dfb
mm <unam3d@gmx.de>
2021-08-13 18:10:33 +0200

    added myAtoi

---
+++ leetCode/MyAtoiClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class MyAtoiClass
+    {
+        public int MyAtoi(string param)
+        {
+            param = param.Trim();
+            if (param == null || param.Length == 0) return 0;
+            var result = 0;
+            char first = param[0];
+            bool isPositiv = true;
+            int startIndex = 0;
+            if (first == '+')
+            {
+                isPositiv = true;
+                startIndex = 1;
+            }
+            else
+            {
+                if (first == '-')
+                {
+                    isPositiv = false;
+                    startIndex = 1;
+                }
+            }
+
+            for (int i = startIndex; i < param.Length; i++)
+            {
+
+                int current = param[i] - 48;
+                if (current < 0 || current > 9) break;
+                if (result > int.MaxValue / 10 || int.MaxValue - current < result * 10)
+                {
+                    return isPositiv ? int.MaxValue : int.MinValue;
+                }
+                result *= 10;
+                result += current;
+            }
+
+            if (!isPositiv)
+                result *= -1;
+
+            return result;
+        }
+    }
+}
---
+++ leetCodeTest/MyAtoiClassTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class MyAtoiClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("123");
+            Assert.IsTrue(result == 123);
+        }
+        [TestMethod]
+        public void MaxInt()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("91283472332");
+            Assert.IsTrue(result == int.MaxValue);
+        }
+        [TestMethod]
+        public void MinInt()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("-91283472332");
+            Assert.IsTrue(result == int.MinValue);
+        }
+        [TestMethod]
+        public void WordsFirst()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("987 words and");
+            Assert.IsTrue(result == 987);
+        }
+        [TestMethod]
+        public void WordsLast()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("words and 987");
+            Assert.IsTrue(result == 0);
+        }
+        [TestMethod]
+        public void NegativInputAndWhitespace()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("    -42");
+            Assert.IsTrue(result == -42);
+        }
+        [TestMethod]
+        public void WhitespaceNegativAndWords()
+        {
+            var instance = new MyAtoiClass();
+            var result = instance.MyAtoi("  -0012a42");
+            Assert.IsTrue(result == -12);
+        }
+        
+    }
+}
