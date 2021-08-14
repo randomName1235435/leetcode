6436bb30bfa978f262864619199bd0a6b3983eaa
mm <unam3d@gmx.de>
2021-08-14 10:54:24 +0200

    added is palindrome

---
+++ leetCode/IsPalindromeClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class IsPalindromeClass
+    {
+        public bool IsPalindrome(int param)
+        {
+            if (param < 0) return false;
+            var length = (int)Math.Floor(Math.Log10(param)) + 1;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                int left = (int)(param / Math.Pow(10, length - 1 - i) % 10);
+                var right = (int)(param / Math.Pow(10, i ) % Math.Pow(10,  i+1) % 10);
+
+                if (left != right)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetCodeTest/IsPalindromeClassTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetCode;
+namespace leetCodeTest
+{
+    [TestClass]
+    public class IsPalindromeClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new IsPalindromeClass();
+            var result = instance.IsPalindrome(121);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void NegativInteger()
+        {
+            var instance = new IsPalindromeClass();
+            var result = instance.IsPalindrome(-121);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void MaxInteger()
+        {
+            var instance = new IsPalindromeClass();
+            var result = instance.IsPalindrome(int.MaxValue);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void ShortInteger()
+        {
+            var instance = new IsPalindromeClass();
+            var result = instance.IsPalindrome(10);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void HighInteger()
+        {
+            var instance = new IsPalindromeClass();
+            var result = instance.IsPalindrome(1223221);
+            Assert.IsTrue(result);
+        }
+    }
+}
