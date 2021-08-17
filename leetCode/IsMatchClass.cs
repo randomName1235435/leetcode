f0f2a8ef2b47f4b8e2d9ee38a20c86d63ff42247
mm <unam3d@gmx.de>
2021-08-17 15:51:09 +0200

    added isMatch (regex)

---
+++ leetCode/IsMatchClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class IsMatchClass
+    {
+        public bool IsMatch(string input, string pattern)
+        {
+            string currentRestPattern = pattern;
+            int inputIndex = 0;
+            
+            while (currentRestPattern.Length != 0 || inputIndex < input.Length)
+            {
+                if (currentRestPattern.Length == 0 && inputIndex < input.Length) return false;
+                if (currentRestPattern.Length > 1 && currentRestPattern[1] == '*')
+                {
+                    if (inputIndex >= input.Length)
+                    {
+                        currentRestPattern = currentRestPattern.Substring(2);
+                        continue;
+                    }
+                    if (MatchWithAesterix(currentRestPattern.Substring(0, 2), input[inputIndex].ToString()))
+                    {
+                        inputIndex++;
+                    }
+                    else
+                    {
+                        currentRestPattern = currentRestPattern.Substring(2);
+                    }
+                }
+                else
+                {
+                    currentRestPattern = currentRestPattern.Substring(1);
+                    if (inputIndex >= input.Length)
+                    {
+                        return false;
+                    }
+                    if (DefaultMatch(currentRestPattern.Substring(0, 1), input[inputIndex].ToString()))
+                        inputIndex++;
+                    else return false;
+                }
+            }
+            return true;
+        }
+
+        private bool DefaultMatch(string pattern, string input)
+        {
+            if (pattern[0] == '.') return input != null && input.Length > 0;
+            return pattern == input;
+        }
+
+        private bool MatchWithAesterix(string pattern, string input)
+        {
+            if (pattern[0] == '.') return input != null && input.Length > 0;
+            return pattern[0].ToString() == input;
+        }
+    }
+}
---
+++ leetCodeTest/IsMatchClassTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetCode;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class IsMatchClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("aa", "a");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AsterixInput()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("aa", "a*");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Any()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("aa", ".*");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void MultipleAsterix()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("aab", "c*a*b");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void MultipleAsterixLonginputFalse()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("mississippi", "mis*is*p*.");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void MultipleAsterixLonginputTrue()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("mississippi", "mis*is*ip*.");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void AnyWithMore()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("ab", ".*c");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void AnyWithMoreSameLiteral()
+        {
+            var instance = new IsMatchClass();
+            var result = instance.IsMatch("aaa", "a*a");
+            Assert.IsTrue(result);
+        }
+    }
+}
