2bdd36c06b959982f09327986ef8302ed5f4e3df
mm <unam3d@gmx.de>
2021-08-17 21:19:40 +0200

    added maxArea

---
+++ leetCode/MaxAreaClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace leetCode
+{
+    public class MaxAreaClass
+    {
+        public int MaxArea(int[] input)
+        {
+            int currentMax = 0;
+            int leftIndex = 0;
+            int rightIndex = input.Length - 1;
+            int currentArea = 0;
+
+            while (leftIndex < rightIndex)
+            {
+                int currentheight = Math.Min(input[leftIndex], input[rightIndex]);
+                int currentWidth = rightIndex - leftIndex;
+                currentArea = currentWidth * currentheight;
+                if (currentArea > currentMax)
+                    currentMax = currentArea;
+                if (input[leftIndex] > input[rightIndex])
+                    rightIndex--;
+                else
+                    leftIndex++;
+            }
+            return currentMax;
+        }
+    }
+}
---
+++ leetCodeTest/MaxAreaClassTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class MaxAreaClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new MaxAreaClass();
+            var result = instance.MaxArea(new int[] { 4, 3, 2, 1, 4 });
+            Assert.IsTrue(result == 16);
+
+        }
+    }
+}
