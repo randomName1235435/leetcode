b803bd348a044e6d602716ccd1ea7b1d8cd3d44e
mm <unam3d@gmx.de>
2021-08-17 22:16:58 +0200

    added RomanToInt, Runtime: 72 ms, faster than 99.32% of C# online submissions; LOL

---
+++ leetCode/RomanToIntClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode
+{
+    public class RomanToIntClass
+    {
+        Dictionary<string, int> literals = new Dictionary<string, int>() {
+        {"M" , 1000   },
+        {"CM", 900    },
+        {"D" , 500    },
+        {"CD", 400    },
+        {"C" , 100    },
+        {"XC", 90     },
+        {"L" , 50     },
+        {"XL", 40     },
+        {"X" , 10     },
+        {"IX", 9      },
+        {"V" , 5      },
+        {"IV", 4      },
+        {"I" , 1      },
+        };
+        public int RomanToInt(string input)
+        {
+            int output = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i + 1 != input.Length && literals.ContainsKey(input.Substring(i, 2)))
+                {
+                    output += literals[input.Substring(i, 2)];
+                    i++;
+                }
+                else
+                    output += literals[input[i].ToString()];
+            }
+            return output;
+        }
+    }
+}
---
+++ leetCodeTest/RomanToIntClassTest.cs
@@ -0,0 +1,24 @@
+﻿using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class RomanToIntClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new RomanToIntClass();
+            var result = instance.RomanToInt("LVIII");
+            Assert.IsTrue(result == 58);
+        }
+        [TestMethod]
+        public void SpecialCaseNine()
+        {
+            var instance = new RomanToIntClass();
+            var result = instance.RomanToInt("IX");
+            Assert.IsTrue(result == 9);
+        }
+    }
+}
