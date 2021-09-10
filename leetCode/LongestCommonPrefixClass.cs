81cc3ab48f38766de58b846bc45fddfd803c78f4
mm <unam3d@gmx.de>
2021-09-10 11:20:35 +0200

    added longestprefix

---
+++ leetCode/LongestCommonPrefixClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class LongestCommonPrefixClass
+    {
+        public string LongestCommonPrefix(string[] input)
+        {
+            if (input == null || input.Length == 0) return "";
+
+            string shortestItem = input.First();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length < shortestItem.Length)
+                    shortestItem = input[i];
+            }
+            if (shortestItem.Length == 0) return "";
+            int j = 1;
+            do
+            {
+                var currentprefix = shortestItem.Substring(0, j);
+                if (input.Any(x => x.Substring(0, j) != currentprefix))
+                {
+                    return shortestItem.Substring(0, --j);
+                }
+                j++;
+            } while (j <= shortestItem.Length);
+            return shortestItem;
+        }
+    }
+}
---
+++ leetCodeTest/LongestCommonPrefixClassTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetCode;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class LongestCommonPrefixClassTest
+    {
+        [TestMethod]
+        public void SimpleInput()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+            Assert.IsTrue(result == "fl");
+        }
+        [TestMethod]
+        public void NoPrefix()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "dog","racecar","car"});
+            Assert.IsTrue(result == "");
+        }
+        [TestMethod]
+        public void FirstEmptyValue()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "", "b" });
+            Assert.IsTrue(result == "");
+        }
+        [TestMethod]
+        public void OnlyEmptyValues()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "" });
+            Assert.IsTrue(result == "");
+        }
+        [TestMethod]
+        public void ShortPrefix()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "ab","a" });
+            Assert.IsTrue(result == "a");
+        }
+        [TestMethod]
+        public void SecondShortPrefix()
+        {
+            var instance = new LongestCommonPrefixClass();
+            var result = instance.LongestCommonPrefix(new string[] { "aa", "ab" });
+            Assert.IsTrue(result == "a");
+        }
+    }
+}
