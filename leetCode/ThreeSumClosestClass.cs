43783736c6c5d75ef5f33f86a8c4b31e1a1d932d
mm <!@!.!>
2023-11-18 18:05:03 +0100

    added threesumclosest

---
+++ leetCode/ThreeSumClosestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class ThreeSumClosestClass
+{
+    public int ThreeSumClosest(int[] numbers, int target)
+    {
+        int firstTry = numbers[0] + numbers[1] + numbers[2];
+
+        Array.Sort(numbers);
+
+        for (int i = 0; i + 2 < numbers.Length; ++i)
+        {
+            if (i > 0 && numbers[i] == numbers[i - 1])
+                continue;
+            int l = i + 1;
+            int r = numbers.Length - 1;
+            while (l < r)
+            {
+                int sum = numbers[i] + numbers[l] + numbers[r];
+                if (sum == target)
+                    return sum;
+                if (Math.Abs(sum - target) < Math.Abs(firstTry - target))
+                    firstTry = sum;
+                if (sum < target)
+                    ++l;
+                else
+                    --r;
+            }
+        }
+
+        return firstTry;
+    }
+}
---
+++ leetCodeTest/ThreeSumClosestClassTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using leetCode;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class ThreeSumClosestClassTest
+    {
+        [TestMethod]
+        public void ThreeSumTest()
+        {
+            ThreeSumClosestClass instance = new ThreeSumClosestClass();
+            var firstParam = new int[] { -1, 2, 1, -4 };
+            var result = instance.ThreeSumClosest(firstParam,1);
+            Assert.IsTrue(result == 2);
+        }
+    }
+}
