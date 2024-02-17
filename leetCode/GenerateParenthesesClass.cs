12b70803dd99c388cd30ed6af65a3c7b46d6b585
mm <!@!.!>
2024-02-17 17:33:18 +0100

    added generateParenthesis

---
+++ leetCode/GenerateParenthesesClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode
+{
+    public class GenerateParenthesesClass
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            Stack<(string str, int open, int close)> stack = new Stack<(string, int, int)>();
+            stack.Push(("", 0, 0));
+
+            while (stack.Count > 0)
+            {
+                (string str, int open, int close) = stack.Pop();
+                if (str.Length == n * 2)
+                {
+                    result.Add(str);
+                }
+                else
+                {
+                    if (open < n)
+                    {
+                        stack.Push((str + "(", open + 1, close));
+                    }
+                    if (close < open)
+                    {
+                        stack.Push((str + ")", open, close + 1));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCodeTest/GenerateParenthesesClassTest.cs
@@ -0,0 +1,17 @@
+﻿using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class GenerateParenthesesClassTest
+    {
+        [TestMethod]
+        public void SimpleValidInput()
+        {
+            var instance = new GenerateParenthesesClass();
+            var result = instance.GenerateParenthesis(1);
+            Assert.IsTrue(result[0] == "()");
+        }
+    }
+}
