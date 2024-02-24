e64e595050aa2251c25bb9207ac6f61722784fef
mm <!@!.!>
2024-02-24 16:47:33 +0100

    added DeleteDuplicates

---
+++ leetCode/DeleteDuplicatesClass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode
+{
+
+    public class DeleteDuplicatesClass
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var hashset = new List<int>();
+
+            ListNode currentNode = head;
+            ListNode before = head;
+            while (currentNode != null)
+            {
+                if (hashset.Contains(currentNode.val))
+                {
+                    before.next = currentNode.next;
+                }
+                else
+                {
+                    hashset.Add(currentNode.val);
+                    before = currentNode;
+                }
+                currentNode = currentNode.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ leetCodeTest/DeleteDuplicatesClassTest.cs
@@ -0,0 +1,24 @@
+﻿using leetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCodeTest
+{
+    [TestClass]
+    public class DeleteDuplicatesClassTest
+    {
+        [TestMethod]
+        public void SimpleValidInput()
+        {
+            var instance = new DeleteDuplicatesClass();
+            var result = instance.DeleteDuplicates(new DeleteDuplicatesClass.ListNode(1,new DeleteDuplicatesClass.ListNode(1)));
+            Assert.IsTrue(result.val == 1 && result.next == null);
+        }
+        [TestMethod]
+        public void TrickyValidInput()
+        {
+            var instance = new DeleteDuplicatesClass();
+            var result = instance.DeleteDuplicates(new DeleteDuplicatesClass.ListNode(1, new DeleteDuplicatesClass.ListNode(1,new DeleteDuplicatesClass.ListNode(1))));
+            Assert.IsTrue(result.val == 1 && result.next == null);
+        }
+    }
+}
